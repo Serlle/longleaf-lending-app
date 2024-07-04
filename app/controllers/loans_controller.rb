0da2c7eefef08d78ceff34117e419cddb0489c6b
Serlle Rosales <serlle.rosales96@gmail.com>
2024-07-04 11:58:35 -0600

    show form of RealEstate model in home page  - created @real_estate in home action to use it in the form rendered in the home view  - created new partial called _real_estate to use turbo after  - puted the render of the partial _real_estate with locals varaible like argument to use form from correct

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -1,4 +1,6 @@
 class LoansController < ApplicationController
   def home
+    # The 'home' action initializes a new RealEstate object
+    @real_estate = RealEstate.new # This object will be used in the form rendered in the home view
   end
 end
